6097124e3304d40c265219f7aec00696036ec5bc
claralainez <109043905+claralainez@users.noreply.github.com>
2022-12-18 17:33:45 -0300

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,38 @@
+
+package com.miaplicacion.primerproyecto4.Controler;
+
+import com.miaplicacion.primerproyecto4.service.IPersonaService;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    @Autowired
+    private IPersonaService persoServ;
+    
+    @PostMapping("/new/persona")
+    public void agregarPersona(@RequestBody Persona pers) {
+        persoServ.crearPersona(pers);
+         }
+    
+    @GetMapping("/ver/personas")
+    @ResponseBody
+    public List<Persona> VerPersonas() {
+        return persoServ.verPersonas();
+    }
+    @DeleteMapping ("/delete/{id}")
+    public void borrarPersona (@PathVariable Long id) {
+    persoServ.borrarPersona(id);
+    }
+    
+}
---
+++ Persona.java
@@ -0,0 +1,38 @@
+
+package com.miaplicacion.primerproyecto4.Controler;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Persona {
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    private String apellido;
+
+   public Persona() {
+                }
+
+    public Persona(Long id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Persona orElse(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
